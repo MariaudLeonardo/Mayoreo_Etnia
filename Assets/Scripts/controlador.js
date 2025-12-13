ef1fed4be1c1cee2b5fcf1f36320f2d04ba875c7
Leonardo Mariaud <mariaudleonardo@gmail.com>
2025-12-12 22:55:20 -0600

    Imagenes de productos 1

--- Assets/Scripts/controlador.js
+++ Assets/Scripts/controlador.js
@@ -1,10 +1,22 @@
 let precioBotinPaqueteSeis = 510;
 let precioBotinPaqueteDoce = 432;
 
+let precioBalerinaPaqueteSeis = 365;
+let precioBalerinaPaqueteDoce = 260;
+
+let precioConfortPaqueteSeis = 453;
+let precioConfortPaqueteDoce = 380;
+
+let precioCasualPaqueteSeis = 315;
+let precioCasualPaqueteDoce = 275;
+
+let precioSandaliaConfortPaqueteSeis = 390;
+let precioSandaliaConfortPaqueteDoce = 310;
+
 // Funciones
 
 /**
- * Establece el precio inicial en el elemento HTML especificado.
+ * Establece el precio inicial (por defecto del paquete de 6) en el elemento HTML especificado.
  * @param {string} id - El ID del elemento <p> del precio.
  */
 function establecerPrecioInicial(id) {
@@ -22,7 +34,7 @@ function establecerPrecioInicial(id) {
     }
 }
 
-// 🔑 CLAVE: Función para ejecutar el código después de que la página se carga (mejor práctica)
+// Añadir aqui todos los precios de cada producto
 document.addEventListener('DOMContentLoaded', (event) => {
     // Llamar la función con el ID correcto de tu HTML
     establecerPrecioInicial('precio_item_01'); 
